c91aea0023ad204931462d9dac01b2d1837a9e81
prasannatuladhar <prasanna.pmt@gmail.com>
2020-04-17 20:27:03 +0545

    Registration/SignUp func

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 def index(request):
     return render(request,'auth_app/index.html',{})
@@ -29,7 +30,45 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request,'auth_app/register.html',{})
+    if request.method == "POST":
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,'Username already taken!')
+                return redirect('register_user')
+            else:
+                   if User.objects.filter(email=email).exists():
+                       messages.error(request,'Email is already been used')
+                       return redirect('register_user')
+                   else:
+                       user = User.objects.create_user(first_name=first_name,last_name=last_name,email=email,password=password,username=username)
+                       user.save()
+                       messages.success(request,"Now you can log in")
+                       return redirect('login_user')
+
+
+
+        else:
+            messages.error(request,'Passwords doesnot matches')
+            return redirect('register_user')    
+
+
+
+
+
+
+
+        
+        
+    else:
+        return render(request,'auth_app/register.html',{})
+    
 
 
     
